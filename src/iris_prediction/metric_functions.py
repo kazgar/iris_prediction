95e96bd535f1a7d74777dc3d62848bd5d2a37f95
Kazik <kazik@example.com>
2025-07-03 17:56:23 -0400

    SVC done

--- src/iris_prediction/metric_functions.py
+++ src/iris_prediction/metric_functions.py
@@ -1,8 +1,9 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
-def calculate_basic_metrics(y_true, y_pred) -> dict:
+def calculate_basic_metrics(y_true, y_pred, model_name) -> dict:
     return {
+        "model_name": model_name,
         "accuracy": accuracy_score(y_true, y_pred),
         "precission": precision_score(y_true, y_pred, average="weighted"),
         "recall": recall_score(y_true, y_pred, average="weighted"),
