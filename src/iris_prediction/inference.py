39c5696d9eebe0a1f6bf8921f8686ff4eca93c5e
Kazik <kazik@example.com>
2025-07-04 11:24:49 -0400

    everythings working

--- src/iris_prediction/inference.py
+++ src/iris_prediction/inference.py
@@ -1,18 +1,29 @@
 import sys
 import time
-from training import trained_models, test_data, y_mapping
+import polars as pl
+from setup.constants import PROJECT_DATA
+from data_prep import expand_dataset, split_x_y, transform_y, split_data
+from load_model import load_model
+import random
 
 def main():
     if len(sys.argv) != 2:
         raise Exception("Usage: python main.py model")
-    model = trained_models[sys.argv[1]]
-    X, y = test_data
-    for dp_x, y_true in zip(X, y):
-        y_pred = model.predict(dp_x)
-        print(f'MODEL PREDICTION -> {y_mapping[y_pred]} || THE TRUTH -> {y_mapping[y_true]}')
-        print(f'MODEL CORRECT: {y_true == y_pred}')
+    iris_df = pl.read_csv(PROJECT_DATA)
+    iris_df_expanded = expand_dataset(iris_df)
+    X, y = split_x_y(iris_df_expanded)
+    y, y_mapping = transform_y(y)
+    y_mapping = {nr: nm for nm, nr in y_mapping.items()}
+    print(y_mapping)
+    X_train, _, X_test, y_train, _, y_test = split_data(
+    X, y, train_size=0.8, val_set=True
+    )
+    model = load_model(sys.argv[1]).fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print('\n')
+    for y_p, y_t in zip(y_pred, y_test):
+        print(f'Model prediction -> {y_mapping[y_p]} || The truth: {y_mapping[y_t]} || VERDICT: {y_p == y_t}\n')
         time.sleep(1)
-
 
 if __name__=='__main__':
     main()
--- src/iris_prediction/load_model.py
+++ src/iris_prediction/load_model.py
@@ -8,52 +8,52 @@ def load_model(
     params = pl.read_csv(PROJECT_ROOT / 'models' / f'{model_name}.csv')
     if model_name == 'lr':
         model = Models().logistic_regression(
-            C=params['C'],
-            max_iter=params['max_iter'],
-            penalty=params['penalty'],
-            solver=params['solver'],
-            tol=params['tol']
+            C=params['C'][0],
+            max_iter=params['max_iter'][0],
+            penalty=params['penalty'][0],
+            solver=params['solver'][0],
+            tol=params['tol'][0]
         )
     
     elif model_name == 'svc':
         model = Models().svc(
-            C=params["C"],
-            degree=params["degree"],
-            gamma=params["gamma"],
-            kernel=params["kernel"],
-            max_iter=params["max_iter"],
+            C=params["C"][0],
+            degree=params["degree"][0],
+            gamma=params["gamma"][0],
+            kernel=params["kernel"][0],
+            max_iter=params["max_iter"][0],
         )    
     
     elif model_name == 'lsvc':
-        model = Models().lsvc(
-            C=params["C"],
-            loss=params["loss"],
-            max_iter=params["max_iter"],
-            multi_class=params["multi_class"],
+        model = Models().linear_svc(
+            C=params["C"][0],
+            loss=params["loss"][0],
+            max_iter=params["max_iter"][0],
+            multi_class=params["multi_class"][0],
         )
     
     elif model_name == 'gbc':
         model = Models().gbc(
-            learning_rate=params["learning_rate"],
-            loss=params["loss"],
-            max_depth=params["max_depth"],
-            n_estimators=params["n_estimators"],
+            learning_rate=params["learning_rate"][0],
+            loss=params["loss"][0],
+            max_depth=params["max_depth"][0],
+            n_estimators=params["n_estimators"][0],
         )
     
     
     elif model_name == 'hgbc':
         model = Models().hgbc(
-            learning_rate=params["learning_rate"],
-            max_iter=params["max_iter"],
-            max_depth=params["max_depth"],
+            learning_rate=params["learning_rate"][0],
+            max_iter=params["max_iter"][0],
+            max_depth=params["max_depth"][0],
         )
 
     elif model_name == 'rf':
         model = Models().rf(
-            criterion=params["criterion"],
-            max_depth=params["max_depth"],
-            max_features=params["max_features"],
-            n_estimators=params["n_estimators"],
+            criterion=params["criterion"][0],
+            max_depth=params["max_depth"][0],
+            max_features=params["max_features"][0],
+            n_estimators=params["n_estimators"][0],
         )
-        
+
     return model
